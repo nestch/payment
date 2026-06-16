6d16e6773bb22ba7b537ec468eaaea6859af6daf
frcangussu <frcangussu@exemplo.com>
2026-06-16 20:54:18 -0300

    mover configuração de URL do banco de dados para settings

--- app/db_nestch.py
+++ app/db_nestch.py
@@ -1,15 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-
-_DB_HOST: str =  os.getenv("DB_HOST", "localhost")
-_DB_PORT: str =  os.getenv("DB_PORT", "3306")
-_DB_NAME: str = os.getenv("DB_NAME", "nestch_db")
-_DB_USER: str = os.getenv("DB_USER", "root")
-_DB_PASSWORD: str =  os.getenv("DB_PASSWORD", "")
-
-_NESTCH_DB_URL = f"mysql+pymysql://{_DB_USER}:{_DB_PASSWORD}@{_DB_HOST}:{_DB_PORT}/{_DB_NAME}"
+from app.config import settings
 
 nestch_engine = create_engine(
     settings.sqlalchemy_database_url,
